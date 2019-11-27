12fa33021e5c3c2e0ad55986e4b3cce27ddbf301
Fidaadzkia <58196125+Fidaadzkia@users.noreply.github.com>
2019-11-27 16:15:09 +0700

    Initial commit

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using utproject.Models;
+using System.Web.Mvc;
+
+namespace utproject.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: movie/random dulu
+        public ActionResult random()
+        {
+            var movie = new Movie() { Name = "shrek!" };
+            return View(movie);
+        }
+    }
+}
---
+++ Models/movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace utproject.Models
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        //get;set; itu buat ngambil nama dan id nya/
+    }
+}
